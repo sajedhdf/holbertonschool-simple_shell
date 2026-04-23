362141a6966dc44703c14346bac47f1cbb26e7ad
Sajed <12205@holbertonstudents.com>
2026-04-23 20:47:44 +0200

    Improve command execution with fork and execve handling

--- execute.c
+++ execute.c
@@ -2,31 +2,54 @@
 #include <sys/wait.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
 extern char **environ;
+
+char *find_command(char *cmd)
+{
+	char *path = getenv("PATH");
+	char *token;
+	static char full[1024];
+
+	token = strtok(path, ":");
+	while (token)
+	{
+		strcpy(full, token);
+		strcat(full, "/");
+		strcat(full, cmd);
+
+		if (access(full, X_OK) == 0)
+			return (full);
+
+		token = strtok(NULL, ":");
+	}
+	return (NULL);
+}
 
 void execute_command(char *command)
 {
 	pid_t pid;
 	char *args[2];
+	char *cmd_path;
 
 	args[0] = command;
 	args[1] = NULL;
 
+	cmd_path = find_command(command);
+	if (!cmd_path)
+		cmd_path = command;
+
 	pid = fork();
-	/* create child process */
 
 	if (pid == 0)
 	{
-		if (execve(command, args, environ) == -1)
+		if (execve(cmd_path, args, environ) == -1)
 		{
 			perror("./hsh");
 			exit(1);
 		}
 	}
 	else
-	{
 		wait(NULL);
-		/* parent waits */
-	}
 }
