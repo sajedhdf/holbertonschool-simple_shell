4ebd45d7d25c49330e137115d270317591dd2a16
Sajed <12205@holbertonstudents.com>
2026-04-23 20:28:51 +0200

    Add header file with function prototypes

--- simple_shell.h
+++ simple_shell.h
@@ -2,12 +2,7 @@
 #define SIMPLE_SHELL_H
 
 void display_prompt(void);
-/* function to display the shell prompt */
-
 char *read_line(void);
-/* function to read the user command */
-
 void execute_command(char *command);
-/* function to execute the command */
 
 #endif
